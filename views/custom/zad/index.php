7441afaad2869b775312e61eb032cb7cacc0a62c
Oceatoon <oceatoon@gmail.com>
2018-10-05 22:19:24 +0400

    some costamazing

---
+++ views/custom/zad/index.php
@@ -0,0 +1,180 @@
+<style type="text/css">
+	#titleCostum{
+		position: relative;
+		top: 350px;
+		left: 0px;
+		background-color: rgba(0,0,0,0.7);
+		color:white;
+		font-size: 44px;
+		font-weight: bolder;
+		z-index: 10;
+		padding: 10px;
+		text-align: center;
+	}
+</style>
+<div id="titleCostum">ZAD : <?php echo $element["name"] ?></div>
+<div class="row margin-top-20  padding-20">
+
+	<div class="col-xs-12 text-center margin-bottom-50">
+	<div id="docCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Round button indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#docCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#docCarousel" data-slide-to="1" class=""></li>
+    <li data-target="#docCarousel" data-slide-to="2" class=""></li>
+    <li data-target="#docCarousel" data-slide-to="3" class=""></li>
+    <li data-target="#docCarousel" data-slide-to="4" class=""></li>
+    <li data-target="#docCarousel" data-slide-to="5" class=""></li>
+    <li data-target="#docCarousel" data-slide-to="6" class=""></li>
+    <li data-target="#docCarousel" data-slide-to="7" class=""></li>
+    <li data-target="#docCarousel" data-slide-to="8" class=""></li>
+    <li data-target="#docCarousel" data-slide-to="9" class=""></li>
+    <li data-target="#docCarousel" data-slide-to="10" class=""></li>
+    <li data-target="#docCarousel" data-slide-to="11" class=""></li>
+    <li data-target="#docCarousel" data-slide-to="12" class=""></li>
+    <li data-target="#docCarousel" data-slide-to="13" class=""></li>
+    <li data-target="#docCarousel" data-slide-to="14" class=""></li>
+    <li data-target="#docCarousel" data-slide-to="15" class=""></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <style type="text/css">
+  	div.item img{margin:auto;}
+  </style>
+  <div class="carousel-inner" role="listbox">
+    <div class="item active"><img class="img-responsive" src='<?php echo Yii::app()->getModule("onepage")->assetsUrl; ?>/images/custom/zad/img3.jpg'> </div>
+    <div class="item"><img class="img-responsive" src='<?php echo Yii::app()->getModule("onepage")->assetsUrl; ?>/images/custom/zad/img4.jpg'> </div>
+    <div class="item"><img class="img-responsive" src='<?php echo Yii::app()->getModule("onepage")->assetsUrl; ?>/images/custom/zad/img7.png'> </div>
+    <div class="item"><img class="img-responsive" src='<?php echo Yii::app()->getModule("onepage")->assetsUrl; ?>/images/custom/zad/img8.jpg'> </div>
+    <div class="item"><img class="img-responsive" src='<?php echo Yii::app()->getModule("onepage")->assetsUrl; ?>/images/custom/zad/img9.jpg'> </div>
+    <div class="item"><img class="img-responsive" src='<?php echo Yii::app()->getModule("onepage")->assetsUrl; ?>/images/custom/zad/img11.jpg'> </div>
+    <div class="item"><img class="img-responsive" src='<?php echo Yii::app()->getModule("onepage")->assetsUrl; ?>/images/custom/zad/img12.jpg'> </div>
+    <div class="item"><img class="img-responsive" src='<?php echo Yii::app()->getModule("onepage")->assetsUrl; ?>/images/custom/zad/img14.jpg'> </div>
+    <div class="item"><img class="img-responsive" src='<?php echo Yii::app()->getModule("onepage")->assetsUrl; ?>/images/custom/zad/img16.jpg'> </div>
+    <div class="item"><img class="img-responsive" src='<?php echo Yii::app()->getModule("onepage")->assetsUrl; ?>/images/custom/zad/img17.jpg'> </div>
+    <div class="item"><img class="img-responsive" src='<?php echo Yii::app()->getModule("onepage")->assetsUrl; ?>/images/custom/zad/img18.jpg'> </div>
+    <div class="item"><img class="img-responsive" src='<?php echo Yii::app()->getModule("onepage")->assetsUrl; ?>/images/custom/zad/img19.jpg'> </div>
+    <div class="item"><img class="img-responsive" src='<?php echo Yii::app()->getModule("onepage")->assetsUrl; ?>/images/custom/zad/img20.jpg'> </div>
+    <div class="item"><img class="img-responsive" src='<?php echo Yii::app()->getModule("onepage")->assetsUrl; ?>/images/custom/zad/img24.jpg'> </div>
+    <div class="item"><img class="img-responsive" src='<?php echo Yii::app()->getModule("onepage")->assetsUrl; ?>/images/custom/zad/img25.jpg'> </div>
+    <div class="item"><img class="img-responsive" src='<?php echo Yii::app()->getModule("onepage")->assetsUrl; ?>/images/custom/zad/img29.jpg'> </div>
+  </div>
+	
+</div>
+		
+	</div>
+
+	<div class="col-xs-12 bgDark">
+		<div class="col-xs-6 padding-20">
+		<h1 class="text-center">BUILD YOUR PROCESS</h1>
+		<div class="col-xs-6 col-md-offset-2 padding-20">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div></div>
+		<div class="col-xs-6"><img class="img-responsive" src='<?php echo Yii::app()->getModule("survey")->assetsUrl; ?>/images/home/sample.png'> </div>
+		</div>
+	</div>
+
+	<div class="col-xs-12 margin-top-20">
+		<div class="col-xs-6"><img class="img-responsive" src='<?php echo Yii::app()->getModule("survey")->assetsUrl; ?>/images/home/evan-dennis-75563-unsplash.jpg'> </div>
+		<div class="col-xs-6 padding-20">
+		<h1 class="text-center">GET ANSWERS TO YOUR QUESTIONS</h1>
+		<div class="col-xs-6 col-md-offset-2 padding-20">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div></div>
+	</div>
+	
+	<div class="col-xs-12">
+		<div class="col-xs-6 padding-20">
+		<h1 class="text-center">CROWD KNOWLEDGE & COLLECTIVE INTELLIGENCE</h1>
+		<div class="col-xs-6 col-md-offset-2 padding-20">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div></div>
+		<div class="col-xs-6"><img class="img-responsive" src='<?php echo Yii::app()->getModule("survey")->assetsUrl; ?>/images/home/edwin-andrade-153753-unsplash.jpg'> </div>
+		</div>
+	</div>
+
+	<div class="col-xs-12">
+		<div class="col-xs-6"><img class="img-responsive" src='<?php echo Yii::app()->getModule("survey")->assetsUrl; ?>/images/home/glen-noble-18012-unsplash.jpg'> </div>
+		<div class="col-xs-6 padding-20">
+		<h1 class="text-center">PROJECT MUTUALISATION & EVALUATIONS </h1>
+		<div class="col-xs-6 col-md-offset-2 padding-20">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div></div>
+	</div>
+
+	<div class="col-xs-12">
+		
+		<div class="col-xs-6 padding-20">
+		<h1 class="text-center"> TOOLS FOR DEMOCRACY </h1>
+		<div class="col-xs-6 col-md-offset-2 padding-20">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div></div>
+		<div class="col-xs-6"><img class="img-responsive" src='<?php echo Yii::app()->getModule("survey")->assetsUrl; ?>/images/home/my-life-through-a-lens-110632-unsplash.jpg'> </div>
+	</div>
+
+	<div class="col-xs-12">
+		<div class="col-xs-6"><img class="img-responsive" src='<?php echo Yii::app()->getModule("survey")->assetsUrl; ?>/images/home/cody-davis-253928-unsplash.jpg'> </div>
+		<div class="col-xs-6 padding-20">
+		<h1 class="text-center">OPEN SOURCE & LIBRE SOFTWARE </h1>
+		<div class="col-xs-6 col-md-offset-2 padding-20">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div></div>
+	</div>
+
+	<div class="col-xs-12">
+		
+		<div class="col-xs-6 padding-20">
+		<h1 class="text-center"> BUILD PARTICIPATIVE COMMUNITIES </h1>
+		<div class="col-xs-6 col-md-offset-2 padding-20">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div></div>
+		<div class="col-xs-6"><img class="img-responsive" src='<?php echo Yii::app()->getModule("survey")->assetsUrl; ?>/images/home/tim-marshall-114623-unsplash.jpg'> </div>
+	</div>
+
+	<div class="col-xs-12">
+		<div class="col-xs-6"><img class="img-responsive" src='<?php echo Yii::app()->getModule("co2")->assetsUrl; ?>/images/logoBtn.png'> </div>
+		<div class="col-xs-6 padding-20">
+		<h1 class="text-center">LOVE & CO </h1>
+		<div class="col-xs-6 col-md-offset-2 padding-20">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div></div>
+	</div>
+
+
+
+
+	
+
+</div>
